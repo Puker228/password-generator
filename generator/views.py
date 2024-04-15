dadf7d05e435982cafbb695a3fe006010161aa6e
SWAGer <131869327+Puker228@users.noreply.github.com>
2024-04-15 19:50:28 +0300

    finish

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,29 @@
+import random
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    characters = list('qwertyuiopasdfghjklzxcvbnm')
+    length = int(request.GET.get('length', 12))
+    the_password = ''
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('QWERTYUIOPASDFGHJKLZXCVBNM'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%'))
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
